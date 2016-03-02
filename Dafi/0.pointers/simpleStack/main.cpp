c94274e65f728b4b92233797f219656eb3e61886
martin-varbanov96 <martin_varbanov96@abv.bg>
2016-03-02 20:07:56 +0200

    stack

---
+++ Dafi/0.pointers/simpleStack/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void getPrintPop(stack<int> stackStruct){
+    if(!getPrintPop().emp){
+    }
+
+}
+
+int main()
+{
+    stack<int> simpleStack;
+    for(int i = 0; i < 5; i++){
+        simpleStack.push(i);
+    }
+
+    simpleStack.push(123);
+}
