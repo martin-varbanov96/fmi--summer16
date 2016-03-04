b99499865db1ffdc249ac920e51ebca5d9db1c12
martin-varbanov96 <martin_varbanov96@abv.bg>
2016-03-04 16:07:25 +0200

    bezu

---
+++ Math/Bezu/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+int getD(int a, int b){
+    int max = 1;
+    int smaller;
+    if(a <= b){
+        smaller = a;
+    }else{
+        smaller = b;
+    }
+    for(int i = 2; i <= smaller; i++){
+        if( (a%i == 0) && (b%i == 0) ){
+            max = i;
+        }
+    }
+    return max;
+}
+
+void printBezu(double a, double b, double d){
+    int sum;
+    for(int u = -b; u <= a; u++ ){
+        for(int v = -b; v <= b; v++){
+            sum = (u*a) + (v*b);
+            if(sum == d){
+                cout << d << " = " << a << " * " << u << " + " << b << " * " << v << endl;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cout << "Enter a =  ";
+    int a;
+    cin >> a;
+    cout << "Enter b = ";
+    int b;
+    cin >> b;
+    int d = getD(a, b);
+    printBezu(a, b ,d);
+}
