42d59d3aa342b9ddfa0eb30a0a6c3358528a599d
martin-varbanov96 <martin_varbanov96@abv.bg>
2016-03-11 18:14:17 +0200

    R

--- Statistics - R/00.SetsOfData/StandartDeviation.R
+++ Statistics - R/00.SetsOfData/StandartDeviation.R
@@ -1,3 +1,14 @@
-a <- c(2, 1, 3, 4, 2, 1, 2, 3, 4, 5, 1234)
+a <- c(2, 1, 3, 4, 2, 1, 2, 3, 4, 5, 20)
 
 deviation <- sd(a)
+print(deviation)
+
+meanValue <- mean(a)
+print(meanValue)
+
+
+barplot(a)
+abline(h = meanValue, col = "red")
+abline( h = deviation, col = "green")
+
+
